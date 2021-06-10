515c8ca37c6523bdf9a19045b7e19eaa98ea28d8
Allaye <kolade199@gmail.com>
2021-06-10 01:30:04 +0000

    final changes now before submission...

--- src/server.js
+++ src/server.js
@@ -3,13 +3,13 @@ const express = require('express');
 const userroutes = require('./routes/user');
 
 const app = express();
-// console.log(path.join(__dirname, '../views'));
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(userroutes);
-const port = process.env.PORT || 9000;
 
+
+const port = process.env.PORT || 9000;
 
 app.listen(port, ()=>{
     console.log(`API is running and listening on port ${port}...`)
